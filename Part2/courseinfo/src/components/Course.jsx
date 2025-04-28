c241d938950dfcd653c791fc4b825f3b0d4bd4c2
Adrianfer4 <yerssondereck@gmail.com>
2025-04-28 16:42:17 -0500

    courseinfo paso 7

--- Part2/courseinfo/src/components/Course.jsx
+++ Part2/courseinfo/src/components/Course.jsx
@@ -4,19 +4,37 @@ const Header = (props) => {
   return <h1>{props.course.name}</h1>;
 };
 
-const Content = (props) => {
-  return <Part course={props.course} />;
-};
-
 const Part = (props) => {
   console.log(props);
   return (
     <div>
-      {props.course.parts.map((part) => (
-        <p key={part.id}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
+      <p>
+        {props.parts.name} {props.parts.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part parts={props.course.parts[0]} />
+      <Part parts={props.course.parts[1]} />
+      <Part parts={props.course.parts[2]} />
+      <Part parts={props.course.parts[3]} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const total = props.course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+  console.log(total);
+  return (
+    <div>
+      <b>total of {total} excercises</b>
     </div>
   );
 };
@@ -26,6 +44,7 @@ const Course = (props) => {
     <div>
       <Header course={props.course} />
       <Content course={props.course} />
+      <Total course={props.course} />
     </div>
   );
 };
